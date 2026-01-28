9717be145e2b4eadaea4d01afee0d44371f9bcff
xtxme <141804351+xtxme@users.noreply.github.com>
2026-01-29 00:04:42 +0700

    edit bug

---
+++ lib/features/home/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/widgets/app_navbar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  void _handleNavTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+      ),
+      bottomNavigationBar: AppNavBar(
+        currentIndex: _currentIndex,
+        onTap: _handleNavTap,
+      ),
+    );
+  }
+}
---
+++ lib/shared/widgets/app_navbar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class AppNavBar extends StatelessWidget {
+  const AppNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  static const Color _selectedIconColor = Color(0xFFDEAF5F);
+  static const Color _unselectedColor = Color(0xFFA48C7E);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.white,
+      selectedItemColor: _unselectedColor,
+      unselectedItemColor: _unselectedColor,
+      showUnselectedLabels: true,
+      selectedFontSize: 12,
+      unselectedFontSize: 12,
+      items: [
+        _NavItem(icon: Icons.home, label: 'Home'),
+        _NavItem(icon: Icons.menu_book, label: 'Subjects'),
+        _NavItem(icon: Icons.assignment, label: 'Assignments'),
+        _NavItem(icon: Icons.calendar_month, label: 'Calendar'),
+        _NavItem(icon: Icons.settings, label: 'Settings'),
+      ],
+    );
+  }
+}
+
+class _NavItem extends BottomNavigationBarItem {
+  _NavItem({required IconData icon, required String label})
+      : super(
+          icon: _IconWidget(icon: icon),
+          activeIcon: _IconWidget(icon: icon, isActive: true),
+          label: label,
+        );
+}
+
+class _IconWidget extends StatelessWidget {
+  const _IconWidget({required this.icon, this.isActive = false});
+  
+  final IconData icon;
+  final bool isActive;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      icon,
+      size: 28,
+      color: isActive ? AppNavBar._selectedIconColor : null,
+    );
+  }
+}
