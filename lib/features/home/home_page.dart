3ddfe9e4528e6437c0e0fca805bb39ef37b38237
xtxme <141804351+xtxme@users.noreply.github.com>
2026-01-29 11:29:00 +0700

    refacter code

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,69 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../shared/widgets/app_navbar.dart';
-import 'providers.dart';
-import '../subjects/subjects_page.dart';
+import '../../shared/widgets/navbar/navbar_shell.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  static const List<String> _titles = [
-    'Home',
-    'Subjects',
-    'Assignments',
-    'Calendar',
-    'Settings',
-  ];
-
-  static const List<Widget> _pages = [
-    _HomeTab(),
-    SubjectsPage(),
-    _PlaceholderTab(title: 'Assignments Page'),
-    _PlaceholderTab(title: 'Calendar Page'),
-    _PlaceholderTab(title: 'Settings Page'),
-  ];
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final currentIndex = ref.watch(currentNavIndexProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_titles[currentIndex]),
-      ),
-      body: IndexedStack(
-        index: currentIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: AppNavBar(
-        currentIndex: currentIndex,
-        onTap: (index) =>
-            ref.read(currentNavIndexProvider.notifier).state = index,
-      ),
-    );
-  }
-}
-
-class _HomeTab extends StatelessWidget {
-  const _HomeTab();
-
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
-    );
-  }
-}
-
-class _PlaceholderTab extends StatelessWidget {
-  const _PlaceholderTab({required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(title),
-    );
+    return const NavbarShell();
   }
 }
